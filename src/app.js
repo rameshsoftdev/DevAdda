0d6fc9c7284c57ddb787f9296c1009643359183e
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2024-09-22 13:33:28 +0530

    Mutiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -1,43 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/ab?c',(req,res)=>{
-    res.send('ab?c route');
-})
-app.get('/pqr+st',(req,res)=>{
-    res.send('pqr+st route');
-})
-app.get('/*pqr',(req,res)=>{
-    res.send('/*pqr route');
-})
-app.get('/aas(sd)+as',(req,res)=>{
-    res.send('aas(sd)+as route');
-})
-app.get('/item',(req,res)=>{
-    res.send(req.query); // query parameters
-})
+// Signature
+// app.use('/route',rh1,rh2,rh3,rh4,rh5)
+// app.use('/route',rh1,rh2,rh3,rh4,rh5)
+// app.use('/route',rh1,[rh2,rh3],rh4,rh5)
 
-app.get('/category/:id/:catname/',(req,res)=>{
-    res.send(req.params); // query parameters
-})
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route 1");
+    next();
+    //    res.send('Route 1');
+  },
+  [(req, res, next) => {
+    // res.send('Route 2');
+    console.log("Route 2");
+    next();
+  },
+  (req, res, munde) => {
+    // res.send('Route 3');
+    console.log("Route 3");
+    munde();
+  }],
+  (req, res) => {
+    res.send("Route 4");
+  }
+);
 
-app.get('/user',(req,res)=>{
-    res.send('GET Method called');
-})
-app.post('/user',(req,res)=>{
-    res.send('POST Method called');
-})
-app.delete('/user',(req,res)=>{
-    res.send('DELETE Method called');
-})
-
-app.put('/user',(req,res)=>{
-    res.send('PUT Method called');
-})
-app.patch('/user',(req,res)=>{
-    res.send('PATCH Method called');
-})
-
-app.listen(3000,(req,res)=>{
-    console.log('Server is succesfully listening on port 3000');
+app.listen(3000, (req, res) => {
+  console.log("Server is succesfully listening on port 3000");
 });
