41868d1524f42a338598e263bdef39808e91af09
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2024-09-22 22:26:57 +0530

    Error handling in routing

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
-const {adminAuth, userAuth} = require('./middlewares/auth')
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use('/admin',adminAuth);
-app.get('/admin/getAllRecords',(req,res)=>{
-    res.send('All recodrs');
+app.use("/admin", adminAuth);
+app.get("/admin/getAllRecords", (req, res) => {
+  try {
+    throw new Error("Something went wrong");
+    res.send("All recodrs");
+  } catch (err) {
+    res.send(err.message);
+  }
 });
-app.delete('/admin/deleteRecords',(req,res)=>{
-    res.send('Deleted Records');
+app.delete("/admin/deleteRecords", (err, req, res, next) => {
+    if(err){
+        res.send("Someting went wrong");
+    }
+  res.send("Deleted Records");
 });
-app.get('/user/Login',(req,res)=>{
-    res.send('User logged in successfully');
+app.get("/user/Login", (req, res) => {
+  res.send("User logged in successfully");
 });
-app.get('/user/getAlluser',userAuth,(req,res)=>{
-    res.send('User Records');
+app.get("/user/getAlluser", userAuth, (req, res) => {
+  res.send("User Records");
 });
-
 
 app.listen(3000, (req, res) => {
   console.log("Server is succesfully listening on port 3000");
