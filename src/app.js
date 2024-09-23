e3bc8a45c79bac14cc0bd2c0f880c47809e2b256
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2024-09-23 23:59:40 +0530

    Created DB, created connected, and inserting data into DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const connectDB = require('./config/database');
+const User = require('./models/User')
 const app = express();
+app.post("/signup",async (req,res)=>{
+    const userData = {
+      firstName:"Pratiksha",
+      lastName:"Rathod",
+      emailId:"pratiksha@gmail.com",
+      password:"Pratiksha@123",
+      age:6,
+      gender:"Female"
+    }
+    try{
+      user = new User(userData);
+    await user.save();
+    res.send("User Created Successfully!");
+    }catch(err){
+      res.send("Error in save :"+err.message);
+    }
+})
 
 connectDB().then(() => {
   console.log('connection established successfully');
