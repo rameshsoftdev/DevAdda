b88d9e1ba9e567ce498d1a1c68eab3d47dab1b54
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2024-09-24 23:25:24 +0530

    Handling get, update, delete, and patch APIs

--- src/app.js
+++ src/app.js
@@ -13,6 +13,42 @@ app.post("/signup", async (req, res) => {
     res.send("Error in save :" + err.message);
   }
 });
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const users = await User.find({});
+    users.length ? res.send(users) : res.send('No Feed found')
+  } catch (err) {
+    res.send("Error in save :" + err.message);
+  }
+});
+app.delete("/user",async (req,res)=>{
+   const userId = req.body.userId;
+   try {
+    const deletedusers = await User.findByIdAndDelete(userId);
+    res.send(deletedusers);
+  } catch (err) {
+    res.send("Error in save :" + err.message);
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    // Ensure updated document is returned by using { new: true }
+    const updatedUser = await User.findByIdAndUpdate(userId, data, { returnDocument:"before" });
+
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send(updatedUser);
+  } catch (err) {
+    res.status(500).send("Error in update: " + err.message);
+  }
+});
+
 
 connectDB()
   .then(() => {
