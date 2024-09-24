81f3095402f7dc86381404ce9f2f359d60f1f164
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2024-09-24 21:12:15 +0530

    Handling dynamic data to be stored in DB via APIs

--- src/app.js
+++ src/app.js
@@ -1,32 +1,26 @@
 const express = require("express");
-const connectDB = require('./config/database');
-const User = require('./models/User')
+const connectDB = require("./config/database");
+const User = require("./models/User");
 const app = express();
-app.post("/signup",async (req,res)=>{
-    const userData = {
-      firstName:"Pratiksha",
-      lastName:"Rathod",
-      emailId:"pratiksha@gmail.com",
-      password:"Pratiksha@123",
-      age:6,
-      gender:"Female"
-    }
-    try{
-      user = new User(userData);
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  const userData = req.body;
+  try {
+    user = new User(userData);
     await user.save();
     res.send("User Created Successfully!");
-    }catch(err){
-      res.send("Error in save :"+err.message);
-    }
-})
-
-connectDB().then(() => {
-  console.log('connection established successfully');
-  app.listen(3000, (req, res) => {
-    console.log("Server is succesfully listening on port 3000");
-  });
-}).catch((err) => {
-  console.error('connection cannot be established');
+  } catch (err) {
+    res.send("Error in save :" + err.message);
+  }
 });
 
-
+connectDB()
+  .then(() => {
+    console.log("connection established successfully");
+    app.listen(3000, (req, res) => {
+      console.log("Server is succesfully listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("connection cannot be established");
+  });
