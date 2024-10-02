040486d56c3688aba35c9103c64e997ee21e2ebe
rameshsoftdev <112886843+rameshsoftdev@users.noreply.github.com>
2024-10-02 20:41:01 +0530

    Handling Schema Validations

--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,46 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName : {
-        type:String
+        type:String,
+        required:true,
+        minlength:4,
+        maxlength:50
     },
     lastName : {
         type:String
     },
     emailId : {
-        type:String
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true
     },
     password : {
-        type:String
+        type:String,
+        required:true
     },
     gender : {
-        type:String
+        type:String,
+        validate(value){
+            if(!["Male","Female","Other"].includes(value)){
+                throw new Error("Please enter valid Gender");
+            }
+        }
+    },
+    photUrl:{
+        type:String,
+        default: "https://images.app.goo.gl/oACHE7phxDF2Z5X19"
     },
     age : {
-        type:Number
+        type:Number,
+        min:18
     },
+    skills: {
+        type:[String]
+    }
+},{
+    timestamps:true
 })
 
 module.exports = mongoose.model("User",userSchema);
